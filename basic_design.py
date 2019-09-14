89967b8cdda332a6cea9a590b530f3fc619a8fd7
Eshita <e.shrawan@gmail.com>
2019-09-14 18:13:53 -0400

    scraped descriptions
    added my first intuition for detecting fake news

--- basic_design.py
+++ basic_design.py
@@ -6,7 +6,8 @@ from bs4 import BeautifulSoup as bs
 import requests
 from tqdm import tqdm
 from sklearn.feature_extraction.text import CountVectorizer
-from torchtext.vocab import GloVe
+from torchtext.vocab import GloVe #to run this code, one will need to install pip packages for
+#each of these functions
 
 import pickle
 
@@ -25,5 +26,35 @@ from sklearn.metrics import confusion_matrix
 with open(os.path.join(basepath, 'train_val_data.pkl'), 'rb') as f:
   train_data, val_data = pickle.load(f)
 
-print('Number of train examples:', len(train_data))
-print('Number of val examples:', len(val_data))
+#for testing downloaded data
+#print('Number of train examples:', len(train_data))
+#print('Number of val examples:', len(val_data))
+
+#basically right now i'm deciding what factors to select. my approach is to test
+#each method individually and work to aggregate it? my research shows the following
+#methods are popular for NLP Analysis
+
+#this gets the description from websites, beecause most fake news websites may not
+#have a descriptor or may be gramatically incorrect
+def get_description_from_html(html):
+  soup = bs(html)
+  description_tag = soup.find('meta', attrs={'name':'og:description'}) or soup.find('meta', attrs={'property':'description'}) or
+  soup.find('meta', attrs={'name':'description'})
+  if description_tag:
+    description = description_tag.get('content') or ''
+  else: # If there is no description, return empty string.
+    description = ''
+  return description
+
+def scrape_description(url):
+  if not url.startswith('http'):
+    url = 'http://' + url #bs works with what it calls valid links, so add http
+  response = requests.get(url, timeout=10)
+  html = response.text
+  description = get_description_from_html(html)
+  return description
+
+#print('Description of Google.com:')
+#print(scrape_description('google.com'))
+
+#implementation of a bag of words model?
